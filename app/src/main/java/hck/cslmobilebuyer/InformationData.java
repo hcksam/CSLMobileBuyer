666ece24c6478beb80a7fc0bf23acad85715a655
hck <hcksam@hotmail.com>
2016-03-22 08:49:24 +0800

    change

---
+++ app/src/main/java/hck/cslmobilebuyer/InformationData.java
@@ -0,0 +1,180 @@
+package hck.cslmobilebuyer;
+
+/**
+ * Created by hck on 21/3/2016.
+ */
+public class InformationData {
+    private boolean isMr;
+    private String lastName;
+    private String firstName;
+    private String contactPhone;
+    private String emailAddr;
+    private String cCHolderName;
+    private String unitNo;
+    private String floorNo;
+    private String buildNo;
+    private String strNo;
+    private String strName;
+    private String deliveryStCatDescSelect;
+    private String areaSelectDelivery;
+    private String districtSelectDelivery;
+    private String sectionSelectDelivery;
+    private String deliveryDateDP;
+    private String timeslotList;
+    
+    public InformationData(){
+        isMr = true;
+        lastName = "CHAN";
+        firstName = "TAI MAN";
+        contactPhone = "51300568";
+        emailAddr = "chy@ibbs.hk";
+        cCHolderName = "CHAN TAI MAN";
+        unitNo = "A";
+        floorNo = "1";
+        buildNo = "";
+        strNo = "123";
+        strName = "HK";
+        deliveryStCatDescSelect = "";
+        areaSelectDelivery = "HK";
+        districtSelectDelivery = "124";
+        sectionSelectDelivery = "ZZZZ";
+        deliveryDateDP = "01/04/2016";
+        timeslotList = "AM1";
+    }
+
+    public boolean isMr() {
+        return isMr;
+    }
+
+    public void setIsMr(boolean isMr) {
+        this.isMr = isMr;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getContactPhone() {
+        return contactPhone;
+    }
+
+    public void setContactPhone(String contactPhone) {
+        this.contactPhone = contactPhone;
+    }
+
+    public String getEmailAddr() {
+        return emailAddr;
+    }
+
+    public void setEmailAddr(String emailAddr) {
+        this.emailAddr = emailAddr;
+    }
+
+    public String getcCHolderName() {
+        return cCHolderName;
+    }
+
+    public void setcCHolderName(String cCHolderName) {
+        this.cCHolderName = cCHolderName;
+    }
+
+    public String getUnitNo() {
+        return unitNo;
+    }
+
+    public void setUnitNo(String unitNo) {
+        this.unitNo = unitNo;
+    }
+
+    public String getFloorNo() {
+        return floorNo;
+    }
+
+    public void setFloorNo(String floorNo) {
+        this.floorNo = floorNo;
+    }
+
+    public String getBuildNo() {
+        return buildNo;
+    }
+
+    public void setBuildNo(String buildNo) {
+        this.buildNo = buildNo;
+    }
+
+    public String getStrNo() {
+        return strNo;
+    }
+
+    public void setStrNo(String strNo) {
+        this.strNo = strNo;
+    }
+
+    public String getStrName() {
+        return strName;
+    }
+
+    public void setStrName(String strName) {
+        this.strName = strName;
+    }
+
+    public String getDeliveryStCatDescSelect() {
+        return deliveryStCatDescSelect;
+    }
+
+    public void setDeliveryStCatDescSelect(String deliveryStCatDescSelect) {
+        this.deliveryStCatDescSelect = deliveryStCatDescSelect;
+    }
+
+    public String getAreaSelectDelivery() {
+        return areaSelectDelivery;
+    }
+
+    public void setAreaSelectDelivery(String areaSelectDelivery) {
+        this.areaSelectDelivery = areaSelectDelivery;
+    }
+
+    public String getDistrictSelectDelivery() {
+        return districtSelectDelivery;
+    }
+
+    public void setDistrictSelectDelivery(String districtSelectDelivery) {
+        this.districtSelectDelivery = districtSelectDelivery;
+    }
+
+    public String getSectionSelectDelivery() {
+        return sectionSelectDelivery;
+    }
+
+    public void setSectionSelectDelivery(String sectionSelectDelivery) {
+        this.sectionSelectDelivery = sectionSelectDelivery;
+    }
+
+    public String getDeliveryDateDP() {
+        return deliveryDateDP;
+    }
+
+    public void setDeliveryDateDP(String deliveryDateDP) {
+        this.deliveryDateDP = deliveryDateDP;
+    }
+
+    public String getTimeslotList() {
+        return timeslotList;
+    }
+
+    public void setTimeslotList(String timeslotList) {
+        this.timeslotList = timeslotList;
+    }
+}
--- app/src/main/java/hck/cslmobilebuyer/MainActivity.java
+++ app/src/main/java/hck/cslmobilebuyer/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     WebView webView;
     Context context;
     String URL;
+    String[] selectedPhones = {"402000923", "402000925", "402000924"};
+    String S7EdgePage = "https://shop.hkt.com/MobOs/stsadditem.html?modelId=609";
+    String informationPage = "https://shop.hkt.com/MobOs/stspersondtl.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +25,23 @@ public class MainActivity extends AppCompatActivity {
 
         context = this;
 
-//        String url = "https://shop.hkt.com/MobOs/stsadditem.html?modelId=609";
-        URL = "https://www.hkcsl.com/tc/online-shop-standalone-handset-price/";
+//        URL = "https://shop.hkt.com/MobOs/stsadditem.html?modelId=609";
+        URL = "https://shop.hkt.com/MobOs/stsadditem.html?modelId=608";
+//        String url = "https://www.hkcsl.com/tc/online-shop-standalone-handset-price/";
 //        String url = "http://www.baidu.com";
 //        String url = "http://ddns.toraou.com:8888/TestHtml/csl%20Online%20Shop.html";
+        CookieSyncManager cookieSyncMngr = CookieSyncManager.createInstance(context);
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.removeAllCookie();
+
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
+//        webView.clearCache(true);
+//        webView.clearHistory();
         webView.loadUrl(URL);
 
-        setWebView(selectPhone());
-
+//        setWebView(selectPhone(selectedPhones[2]));
+        setWebView(null);
     }
 
     public void setWebView(final String javaScript){
@@ -40,13 +52,14 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageFinished(WebView view, String url) {
+//                Toast.makeText(context, url, Toast.LENGTH_LONG).show();
                 if (url.equalsIgnoreCase(URL)) {
-                    webView.loadUrl("javascript:(function() { " +
-                            forword() +
+                    view.loadUrl("javascript:(function() { " +
+                            selectPhone("402000922") +
                             "})()");
-                } else {
-                    webView.loadUrl("javascript:(function() { " +
-                            selectPhone() +
+                }else if (url.equalsIgnoreCase(informationPage)){
+                    view.loadUrl("javascript:(function() { " +
+                            inputInformation(new InformationData()) +
                             "})()");
                 }
             }
@@ -59,24 +72,111 @@ public class MainActivity extends AppCompatActivity {
         return javaScript;
     }
 
-    private String forword(){
+    private String forward(){
         String javaScript = "window.location.href = 'https://shop.hkt.com/MobOs/stsadditem.html?modelId=609';";
         return javaScript;
     }
 
-    private String selectPhone(){
-        String javaScript = "";
-//        String javaScript = "var color = $('input:radio[name=color]');";
-//        javaScript += "var price = $('input:radio[name=promoTypeId]');";
-        javaScript += "$('body').append('test');";
-        javaScript += "$('body').append($(\"input[type='radio'][name='promoTypeId']\").size());";
-//        javaScript += "if(color.is(':checked') === false) {";
+    private String selectPhone(String selectedPhone){
+//        Toast.makeText(context, "selectPhone", Toast.LENGTH_LONG).show();
+//        String javaScript = "";
+        String javaScript = "var color = $('input[type=radio][name=color]');";
+        javaScript += "var price = $('input[type=radio][name=promoTypeId]');";
+//        javaScript += "$('body').append('test');";
+//        javaScript += "$('body').append( $('#dialog-promoCode').parent().css('display'));";
+//        javaScript += "$('body').append($(\"input[type='radio'][name='promoTypeId']\").size());";
+//        javaScript += "$('body').append(color.filter('[value=402000924]').val());";
+        javaScript += "color.each(function( index ) {";
+        javaScript += "if ($(this).val() !=  "+selectedPhone+"){";
+        javaScript += "$(this).prop('checked', false);";
+        javaScript += "}else{";
+        javaScript += "$(this).prop('checked', true);";
+        javaScript += "}";
+        javaScript += "});";
+//        javaScript += "color.filter('[value=402000923]').prop('checked', false);";
 //        javaScript += "color.filter('[value=402000924]').prop('checked', true);";
-//        javaScript += "}";
+        javaScript += "price.filter('[value=0]').prop('checked', false);";
+        javaScript += "price.filter('[value=1]').prop('checked', true);";
 //        javaScript += "if(price.is(':checked') === false) {";
 //        javaScript += "price.filter('[value=1]').prop('checked', true);";
 //        javaScript += "}";
-//        javaScript += "$('#index-form').submit();";
+        javaScript += "var runSAHS = function () {";
+        javaScript += "var r = $.Deferred();";
+        javaScript += "$('#sahsButton').click();";
+        javaScript += "setTimeout(function () {r.resolve();}, 1000);";
+        javaScript += "return r;";
+        javaScript += "};";
+
+        javaScript += "var runPromo = function () {";
+        javaScript += "var r = $.Deferred();";
+//        javaScript += "$('body').append('test');";
+//        javaScript += "$('body').append( $('#dialog-promoCode').parent().css('display'));";
+        javaScript += "if ($('#dialog-promoCode').parent().css('display') != 'none'){";
+        javaScript += "$('#promoCode').val('SPECIAL');";
+        javaScript += "$('#submitButton').click();";
+        javaScript += "}";
+        javaScript += "setTimeout(function () {r.resolve();}, 1000);";
+        javaScript += "return r;";
+        javaScript += "};";
+
+        javaScript += "var runCheckout = function () {";
+        javaScript += "var r = $.Deferred();";
+        javaScript += "if ($('#dialog-cart').parent().css('display') != 'none'){";
+        javaScript += "$('#checkoutButton').click();";
+        javaScript += "}";
+        javaScript += "setTimeout(function () {r.resolve();}, 1000);";
+        javaScript += "return r;";
+        javaScript += "};";
+
+//        javaScript += "runSAHS().done(runPromo().done(runCheckout));";
+        javaScript += "runSAHS().done(runPromo);";
+        javaScript += "runPromo().done(runCheckout);";
+
+        return javaScript;
+    }
+
+    private String inputInformation(InformationData data){
+//        String javaScript = "$('#customer.title"+((data.isMr())? "1":"2")+"').prop('checked', true);";
+        String javaScript = "$('input[value="+((data.isMr())? "Mr":"Ms")+"]').prop('checked', true);";
+        javaScript += "$('#lastName').val('"+data.getLastName()+"');";
+        javaScript += "$('#firstName').val('"+data.getFirstName()+"');";
+        javaScript += "$('#contactPhone').val('"+data.getContactPhone()+"');";
+        javaScript += "$('#emailAddr').val('"+data.getEmailAddr()+"');";
+        javaScript += "$('#confirmEmailAddr').val('"+data.getEmailAddr()+"');";
+        javaScript += "$('#cCHolderName').val('"+data.getcCHolderName()+"');";
+        javaScript += "$('#unitNo').val('"+data.getUnitNo()+"');";
+        javaScript += "$('#floorNo').val('"+data.getFloorNo()+"');";
+        javaScript += "$('#buildNo').val('"+data.getBuildNo()+"');";
+        javaScript += "$('#strNo').val('"+data.getStrNo()+"');";
+        javaScript += "$('#strName').val('"+data.getStrName()+"');";
+        javaScript += "$('#deliveryStCatDescSelect').val('"+data.getDeliveryStCatDescSelect()+"');";
+
+        javaScript += "var selectArea = function () {";
+        javaScript += "var r = $.Deferred();";
+        javaScript += "$('#areaSelectDelivery').val('"+data.getAreaSelectDelivery()+"');";
+//        javaScript += "$('#areaSelectDelivery').change();";
+        javaScript += "setTimeout(function () {r.resolve();}, 2500);";
+        javaScript += "return r;";
+        javaScript += "};";
+
+        javaScript += "var selectDistrict = function () {";
+        javaScript += "var r = $.Deferred();";
+        javaScript += "$('#districtSelectDelivery').val('"+data.getDistrictSelectDelivery()+"');";
+        javaScript += "setTimeout(function () {r.resolve();}, 2500);";
+        javaScript += "return r;";
+        javaScript += "};";
+
+        javaScript += "var selectSection = function () {";
+        javaScript += "var r = $.Deferred();";
+        javaScript += "$('#sectionSelectDelivery').val('"+data.getSectionSelectDelivery()+"');";
+        javaScript += "$('#deliveryDateDP').val('"+data.getDeliveryDateDP()+"');";
+        javaScript += "$('#timeslotList').val('"+data.getTimeslotList()+"');";
+        javaScript += "$('input[type=checkbox]').prop('checked', true);";
+        javaScript += "setTimeout(function () {r.resolve();}, 2500);";
+        javaScript += "return r;";
+        javaScript += "};";
+
+        javaScript += "selectArea().done(selectDistrict().done(selectSection));";
         return javaScript;
     }
 
