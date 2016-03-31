5c674bbcb0594bdb682618960555d6352f92510d
hck <hcksam@hotmail.com>
2016-04-01 06:41:16 +0800

    change to use WebChromeClient

--- app/src/main/java/hck/cslmobilebuyer/MainActivity.java
+++ app/src/main/java/hck/cslmobilebuyer/MainActivity.java
@@ -11,10 +11,8 @@ import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.JavascriptInterface;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -36,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout functionBar;
 
     String mainUrl;
+    String loadingUrl;
     String selectedPhone;
     String selectedColor;
 //    String[] selectedPhones = {"402000923", "402000925", "402000924"};
@@ -45,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
     final String conformPage = "https://shop.hkt.com/MobOs/stsconfirmation.html";
     HashMap<String, String> phoneMap = getPhoneMap();
     HashMap<String, HashMap<String, String>> phoneColorMap = getPhoneColorMap();
+    HashMap<String, Integer> loadingCount = new HashMap<>();
 
     private HashMap<String, String> getPhoneMap(){
         HashMap<String, String> phoneMap = new HashMap<>();
@@ -220,100 +220,131 @@ public class MainActivity extends AppCompatActivity {
 //    };
 
     public void setWebView(){
+        loadingUrl = mainUrl;
+        loadingCount = new HashMap<>();
+        loadingCount.put(mainUrl, 0);
+        loadingCount.put(informationPage, 0);
+        loadingCount.put(conformPage, 0);
+
         webView.loadUrl(mainUrl);
-//        webView.setWebChromeClient(new WebChromeClient() {
-//            @Override
-//            public void onProgressChanged (WebView view, int newProgress){
-//                String url = view.getUrl();
-//                if (newProgress == 100){
-//                    if (url.equalsIgnoreCase(mainUrl)) {
-//                        loadJavaScript(view, selectPhone() + selectPhoneSubmit());
-//                    } else if (url.equalsIgnoreCase(informationPage)) {
-//                        loadJavaScript(view, inputInformation());
-//                    } else if (url.equalsIgnoreCase(conformPage)) {
-//                        loadJavaScript(view, conformInformation());
-//                    } else {
-//                        Log.w("hck new url", url);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged (WebView view, int newProgress){
+                String url = view.getUrl();
+                if (newProgress == 100){
+                    int count = loadingCount.get(url);
+
+                    if (count == 0) {
+                        if (url.equalsIgnoreCase(mainUrl)) {
+                            loadJavaScript(view, selectPhone() + selectPhoneSubmit());
+                        } else if (url.equalsIgnoreCase(informationPage)) {
+                            loadJavaScript(view, inputInformation());
+                        } else if (url.equalsIgnoreCase(conformPage)) {
+                            loadJavaScript(view, conformInformation());
+                        } else {
+                            Log.w("hck new url", url);
 //                        Toast.makeText(context, url, Toast.LENGTH_LONG).show();
-//                    }
-//                }
+                        }
+                        loadingCount.put(url, ++count);
+                    }else{
+                        Toast.makeText(context, url+"\n"+count, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+//        webView.setWebViewClient(new WebViewClient() {
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+////                loadingUrl = url;
+////                if (url.equalsIgnoreCase(mainUrl)) {
+////                    loadJavaScript(view, selectPhone() + selectPhoneSubmit());
+////                } else if (url.equalsIgnoreCase(informationPage)) {
+////                    loadJavaScript(view, inputInformation());
+////                } else if (url.equalsIgnoreCase(conformPage)) {
+////                    loadJavaScript(view, conformInformation());
+////                } else {
+////                    Log.w("hck new url", url);
+////                    Toast.makeText(context, url, Toast.LENGTH_LONG).show();
+////                }
+//                super.onPageFinished(view, url);
 //            }
 //        });
-        webView.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                view.loadUrl(url);
-//                return true;
+
+//        webView.setWebViewClient(new WebViewClient() {
+////            @Override
+////            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+////                view.loadUrl(url);
+////                return true;
+////
+////            }
 //
-//            }
-
-//            @Override
-//            public void onLoadResource(WebView view, String url){
-//            }
-
-//            @Override
-//            public void onPageStarted(WebView view, String url, Bitmap favicon){
+////            @Override
+////            public void onLoadResource(WebView view, String url){
+////            }
 //
-//            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-//                Toast.makeText(context, url, Toast.LENGTH_LONG).show();
-                if (url.equalsIgnoreCase(mainUrl)) {
-//                    view.loadUrl("javascript:(function() { " +
-//                            selectPhone("402000922") +
-//                            "})()");
-                    loadJavaScript(view, selectPhone() + selectPhoneSubmit());
-                } else if (url.equalsIgnoreCase(informationPage)) {
-//                    view.loadUrl("javascript:(function() { " +
-//                            inputInformation(new InformationData()) +
-//                            "})()");
-                    loadJavaScript(view, inputInformation());
-                } else if (url.equalsIgnoreCase(conformPage)) {
-                    loadJavaScript(view, conformInformation());
-//                    view.buildDrawingCache();
-//                    Bitmap bitmap = getBitmapFromURL("https://shop.hkt.com/MobOs/captcha.html");
-//                    Bitmap bitmap = getBitmapFromURL2("http://cdn1.theodysseyonline.com/files/2015/12/20/635861833670816810507191518_6670-perfect-snow-1920x1080-nature-wallpaper.jpg");
-//                    captchaImage.setImageBitmap(bitmap);
-//                    new ImageLoadTask("http://cdn1.theodysseyonline.com/files/2015/12/20/635861833670816810507191518_6670-perfect-snow-1920x1080-nature-wallpaper.jpg", captchaImage).execute();
-//                    new ImageLoadTask("https://shop.hkt.com/MobOs/captcha.html", captchaImage).execute();
-//                    String javascript = "javascript: var form = document.getElementsByClassName('form');"
-//                            + "var body = document.getElementsByTagName('body');"
-//                            + "body[0].innerHTML = form[0].innerHTML;;";
-//                    view.loadUrl(javascript);
-//                    view.setVisibility(View.VISIBLE);
-//                    view.loadUrl("javascript:(function() { " +
-//                            "$('body').html($('img#captcha').html());" +
-//                            "})()");
-
-                    //THIS PIECE OF CODE RETURNING NULL BITMAP
-//                    view.setDrawingCacheEnabled(true);
+////            @Override
+////            public void onPageStarted(WebView view, String url, Bitmap favicon){
+////
+////            }
+//
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+////                Toast.makeText(context, url, Toast.LENGTH_LONG).show();
+//                if (url.equalsIgnoreCase(mainUrl)) {
+////                    view.loadUrl("javascript:(function() { " +
+////                            selectPhone("402000922") +
+////                            "})()");
+//                    loadJavaScript(view, selectPhone() + selectPhoneSubmit());
+//                } else if (url.equalsIgnoreCase(informationPage)) {
+////                    view.loadUrl("javascript:(function() { " +
+////                            inputInformation(new InformationData()) +
+////                            "})()");
+//                    loadJavaScript(view, inputInformation());
+//                } else if (url.equalsIgnoreCase(conformPage)) {
+//                    loadJavaScript(view, conformInformation());
 ////                    view.buildDrawingCache();
-//                    Bitmap bmap = view.getDrawingCache();
-//                    captchaImage.setImageBitmap(bmap);
-
-                } else {
-                    Log.w("hck new url", url);
-                    Toast.makeText(context, url, Toast.LENGTH_LONG).show();
-                }
-                super.onPageFinished(view, url);
-            }
-
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
-                Toast.makeText(context, "Received error " + error, Toast.LENGTH_LONG).show();
-            }
-
-//            @Override
-//            public void onReceivedSslError(WebView view,
-//                                           SslErrorHandler handler, SslError error) {
-//                // TODO Auto-generated method stub
-//                super.onReceivedSslError(view, handler, error);
-//                Toast.makeText(context, "Received error "+error, Toast.LENGTH_LONG).show();
+////                    Bitmap bitmap = getBitmapFromURL("https://shop.hkt.com/MobOs/captcha.html");
+////                    Bitmap bitmap = getBitmapFromURL2("http://cdn1.theodysseyonline.com/files/2015/12/20/635861833670816810507191518_6670-perfect-snow-1920x1080-nature-wallpaper.jpg");
+////                    captchaImage.setImageBitmap(bitmap);
+////                    new ImageLoadTask("http://cdn1.theodysseyonline.com/files/2015/12/20/635861833670816810507191518_6670-perfect-snow-1920x1080-nature-wallpaper.jpg", captchaImage).execute();
+////                    new ImageLoadTask("https://shop.hkt.com/MobOs/captcha.html", captchaImage).execute();
+////                    String javascript = "javascript: var form = document.getElementsByClassName('form');"
+////                            + "var body = document.getElementsByTagName('body');"
+////                            + "body[0].innerHTML = form[0].innerHTML;;";
+////                    view.loadUrl(javascript);
+////                    view.setVisibility(View.VISIBLE);
+////                    view.loadUrl("javascript:(function() { " +
+////                            "$('body').html($('img#captcha').html());" +
+////                            "})()");
 //
+//                    //THIS PIECE OF CODE RETURNING NULL BITMAP
+////                    view.setDrawingCacheEnabled(true);
+//////                    view.buildDrawingCache();
+////                    Bitmap bmap = view.getDrawingCache();
+////                    captchaImage.setImageBitmap(bmap);
+//
+//                } else {
+//                    Log.w("hck new url", url);
+//                    Toast.makeText(context, url, Toast.LENGTH_LONG).show();
+//                }
+//                super.onPageFinished(view, url);
 //            }
-        });
+//
+//            @Override
+//            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+//                super.onReceivedError(view, request, error);
+//                Toast.makeText(context, "Received error " + error, Toast.LENGTH_LONG).show();
+//            }
+//
+////            @Override
+////            public void onReceivedSslError(WebView view,
+////                                           SslErrorHandler handler, SslError error) {
+////                // TODO Auto-generated method stub
+////                super.onReceivedSslError(view, handler, error);
+////                Toast.makeText(context, "Received error "+error, Toast.LENGTH_LONG).show();
+////
+////            }
+//        });
     }
 
     private void loadJavaScript(WebView view, String javaScript){
@@ -417,22 +448,22 @@ public class MainActivity extends AppCompatActivity {
         String javaScript = "";
 
         javaScript += "$('#sahsButton').click();";
-        javaScript += "var checkoutInterval = null;";
+//        javaScript += "var checkoutInterval = null;";
 
-        javaScript += "var promoInterval = setInterval(function () {";
-        javaScript += "if ($('#dialog-promoCode').parent().css('display') != 'none'){";
+//        javaScript += "var promoInterval = setInterval(function () {";
+//        javaScript += "if ($('#dialog-promoCode').parent().css('display') != 'none'){";
         javaScript += "$('#promoCode').val('SPECIAL');";
         javaScript += "$('#submitButton').click();";
-        javaScript += "clearInterval(promoInterval);";
-        javaScript += "}";
-        javaScript += "},2000);";
+//        javaScript += "clearInterval(promoInterval);";
+//        javaScript += "}";
+//        javaScript += "},2000);";
 
-        javaScript += "checkoutInterval = setInterval(function () {";
-        javaScript += "if ($('#dialog-cart').parent().css('display') != 'none'){";
+//        javaScript += "checkoutInterval = setInterval(function () {";
+//        javaScript += "if ($('#dialog-cart').parent().css('display') != 'none'){";
         javaScript += "$('#checkoutButton').click();";
-        javaScript += "clearInterval(checkoutInterval);";
-        javaScript += "}";
-        javaScript += "},2000);";
+//        javaScript += "clearInterval(checkoutInterval);";
+//        javaScript += "}";
+//        javaScript += "},2000);";
 
         return javaScript;
     }
@@ -463,42 +494,42 @@ public class MainActivity extends AppCompatActivity {
         javaScript += "$('#timeslotList').val('"+data.getTimeslotList()+"');";
         javaScript += "$('input[type=checkbox]').prop('checked', true);";
 
-        javaScript += "var StCatInterval = setInterval(function () {";
-        javaScript += "if ($('#deliveryStCatDescSelect').val() != '"+data.getDeliveryStCatDescSelect()+"'){";
+//        javaScript += "var StCatInterval = setInterval(function () {";
+//        javaScript += "if ($('#deliveryStCatDescSelect').val() != '"+data.getDeliveryStCatDescSelect()+"'){";
         javaScript += "$('#deliveryStCatDescSelect').val('"+data.getDeliveryStCatDescSelect()+"').change();";
-        javaScript += "}else{";
-        javaScript += "clearInterval(StCatInterval);";
-        javaScript += "}";
-        javaScript += "},2000);";
+//        javaScript += "}else{";
+//        javaScript += "clearInterval(StCatInterval);";
+//        javaScript += "}";
+//        javaScript += "},2000);";
 
-        javaScript += "var areaInterval = setInterval(function () {";
-        javaScript += "if ($('#areaSelectDelivery').val() != '"+data.getAreaSelectDelivery()+"'){";
+//        javaScript += "var areaInterval = setInterval(function () {";
+//        javaScript += "if ($('#areaSelectDelivery').val() != '"+data.getAreaSelectDelivery()+"'){";
         javaScript += "$('#areaSelectDelivery').val('"+data.getAreaSelectDelivery()+"').change();";
-        javaScript += "}else{";
-        javaScript += "clearInterval(areaInterval);";
-        javaScript += "}";
-        javaScript += "},2000);";
+//        javaScript += "}else{";
+//        javaScript += "clearInterval(areaInterval);";
+//        javaScript += "}";
+//        javaScript += "},2000);";
 
-        javaScript += "var districtInterval = setInterval(function () {";
-        javaScript += "if ($('#districtSelectDelivery').val()!= '"+data.getDistrictSelectDelivery()+"'){";
+//        javaScript += "var districtInterval = setInterval(function () {";
+//        javaScript += "if ($('#districtSelectDelivery').val()!= '"+data.getDistrictSelectDelivery()+"'){";
         javaScript += "$('#districtSelectDelivery').val('"+data.getDistrictSelectDelivery()+"').change();";
-        javaScript += "}else{";
-        javaScript += "clearInterval(districtInterval);";
-        javaScript += "}";
-        javaScript += "},2000);";
+//        javaScript += "}else{";
+//        javaScript += "clearInterval(districtInterval);";
+//        javaScript += "}";
+//        javaScript += "},2000);";
 
-        javaScript += "var sectionInterval = setInterval(function () {";
-        javaScript += "if ($('#sectionSelectDelivery').val()!= '"+data.getSectionSelectDelivery()+"' || $('#timeslotList').val() != '"+data.getTimeslotList()+"'){";
+//        javaScript += "var sectionInterval = setInterval(function () {";
+//        javaScript += "if ($('#sectionSelectDelivery').val()!= '"+data.getSectionSelectDelivery()+"' || $('#timeslotList').val() != '"+data.getTimeslotList()+"'){";
         javaScript += "$('#sectionSelectDelivery').val('"+data.getSectionSelectDelivery()+"').change();";
         javaScript += "$('#deliveryDateDP').val('"+data.getDeliveryDateDP()+"');";
         javaScript += "$('#timeslotList').val('"+data.getTimeslotList() +"').change();";
         javaScript += "$('input[type=checkbox]').prop('checked', true);";
-        javaScript += "}else{";
-        javaScript += "clearInterval(sectionInterval);";
+//        javaScript += "}else{";
+//        javaScript += "clearInterval(sectionInterval);";
         javaScript += "$('button[name=submit]').focus();";
         javaScript += "Android.showToast('Auto fill complete!');";
-        javaScript += "}";
-        javaScript += "},2000);";
+//        javaScript += "}";
+//        javaScript += "},2000);";
 
 //        javaScript += "var submitInterval = setInterval(function () {";
 //        javaScript += "if ($('#deliveryStCatDescSelect').val() == '"+data.getDeliveryStCatDescSelect()+"' && $('#areaSelectDelivery').val() == '"+data.getAreaSelectDelivery()+"' && $('#districtSelectDelivery').val() == '"+data.getDistrictSelectDelivery()+"' && $('#sectionSelectDelivery').val() == '"+data.getSectionSelectDelivery()+"' && $('#timeslotList').val() == '"+data.getTimeslotList()+"'){";
@@ -620,7 +651,7 @@ public class MainActivity extends AppCompatActivity {
         /** Show a toast from the web page */
         @JavascriptInterface
         public void showToast(String toast) {
-            Toast.makeText(mContext, toast, Toast.LENGTH_LONG).show();
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
         }
     }
 }
